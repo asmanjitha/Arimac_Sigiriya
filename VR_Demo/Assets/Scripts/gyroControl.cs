f163c72552bc172724bde8b65961b8515507b2a7
asmanjitha <asmanjitha@gmail.com>
2018-06-26 08:41:41 +0530

    first commit

---
+++ VR_Demo/Assets/Scripts/gyroControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gyroControl : MonoBehaviour {
+
+	// Use this for initialization
+
+	private bool gyroEnabled;
+	private Gyroscope gyro;
+
+	private GameObject cameraContainer;
+	private Quaternion rot;
+
+
+	void Start () {
+
+		cameraContainer = new GameObject("Camera COntainer")
+		gyroEnabled = EnableGyro()
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gyro.attitude
+	}
+
+	private bool EnableGyro()
+	{
+		if (SystemInfo.supportsGyroscope)
+		{
+			gyro = Input.gyro;
+			gyro.enabled = true;
+			return true;
+		}
+
+		return false;
+	}
+}
